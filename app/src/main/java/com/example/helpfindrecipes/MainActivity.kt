0802ad229b5a89b4dd391f8ec0f821f34a5cec08
Margooo <margaritamarcevskaa13@gmail.com>
2025-03-29 12:36:26 +0300

    add list of items (added - RecipeAdapter.kt, list_item.xml, Recipes.kt)

--- app/src/main/java/com/example/helpfindrecipes/MainActivity.kt
+++ app/src/main/java/com/example/helpfindrecipes/MainActivity.kt
@@ -1,47 +1,71 @@
 package com.example.helpfindrecipes
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.helpfindrecipes.adapter.RecipeAdapter
+import com.example.helpfindrecipes.databinding.ActivityMainBinding
 import com.example.recipes.retrofit.RecipeApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: RecipeAdapter
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val tv = findViewById<TextView>(R.id.tv)
-        val b = findViewById<Button>(R.id.button)
+
+
+        adapter = RecipeAdapter()
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        binding.rcView.adapter = adapter
+
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+//        val tv = findViewById<TextView>(R.id.tv)
+//        val b = findViewById<Button>(R.id.button)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://dummyjson.com")
+            .baseUrl("https://dummyjson.com").client(client)
             .addConverterFactory(GsonConverterFactory.create()).build()
         val recipeApi = retrofit.create(RecipeApi::class.java)
 
-
-        b.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch() {
-                val recipe = recipeApi.getRecipeById()
-                runOnUiThread {
-                    tv.text = recipe.name
+        CoroutineScope(Dispatchers.IO).launch() {
+           val list = recipeApi.getAllRecipes()
+//            val recipe = recipeApi.getRecipeById(3)
+            runOnUiThread {
+                binding.apply{
+                    adapter.submitList(list.recipes)
                 }
             }
         }
+
+//        binding.button.setOnClickListener {
+//            CoroutineScope(Dispatchers.IO).launch() {
+//                val recipe = recipeApi.getRecipeById(3)
+//                runOnUiThread {
+//                    tv.text = recipe.name
+//                }
+//            }
+//        }
 
 
 
--- app/src/main/java/com/example/helpfindrecipes/retrofit/RecipeApi.kt
+++ app/src/main/java/com/example/helpfindrecipes/retrofit/RecipeApi.kt
@@ -1,9 +1,17 @@
 package com.example.recipes.retrofit
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface RecipeApi {
-    @GET("/recipes/1")
-    suspend fun getRecipeById(): Recipe
+    @GET("recipes/{id}")
+    suspend fun getRecipeById(@Path("id") id: Int): Recipe
 
+//    @POST("auth/login")
+//    suspend fun auth(@Body authRequest: AuthRequest): User
+
+    @GET("recipes")
+    suspend fun getAllRecipes(): Recipes
 }
